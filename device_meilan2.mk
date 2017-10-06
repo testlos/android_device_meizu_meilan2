4a6f177d3b33893ba6e9eb64a1ba79ba570b7e8c
adi766 <adityakumarteli7846@gmail.com>
2017-10-06 14:01:46 +0000

    Update

--- device_meilan2.mk
+++ device_meilan2.mk
@@ -3,9 +3,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
-$(call inherit-product-if-exists, vendor/meizu/M2/M2-vendor.mk)
-
 $(call inherit-product-if-exists, vendor/meizu/meilan2/meilan2-vendor.mk)
+
+$(call inherit-product-if-exists, vendor/meizu/meilan2/M2-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/meizu/meilan2/overlay
 
